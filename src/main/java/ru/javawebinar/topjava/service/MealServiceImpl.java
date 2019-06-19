8abeed70cdbc1b7221668126494b174fd404fb90
Marina Potapova <marinapotapova08@gmail.com>
2019-06-19 00:19:39 -0500

    HW02 Реализация 'MealRestController

--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -1,9 +1,51 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MealServiceImpl implements MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
 
+    @Autowired
+    public MealServiceImpl(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Meal create(Meal meal) {
+        return repository.save(meal);
+    }
+
+    @Override
+    public void delete(int id) throws NotFoundException {
+      repository.delete(id);
+    }
+
+    @Override
+    public Meal get(int id) throws NotFoundException {
+        return repository.get(id);
+    }
+
+    @Override
+    public void update(Meal meal, int id) throws NotFoundException {
+
+    }
+
+    @Override
+    public List<MealTo> getAll() {
+        return new ArrayList<>(MealsUtil.getWithExcess(repository.getAll(), MealsUtil.DEFAULT_CALORIES_PER_DAY));
+    }
+
+    @Override
+    public List<MealTo> getAllByDateTime() {
+        return null;
+    }
 }
